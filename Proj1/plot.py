cdb43da73e5844d517ac441937ce4c463ed9a2d7
Jiajun Yu <55095307+Yu709806736@users.noreply.github.com>
2022-05-14 00:47:00 -0700

    Update plot.py

--- Proj1/plot.py
+++ Proj1/plot.py
@@ -13,24 +13,29 @@ puzzles = {0: [[1, 2, 3], [4, 5, 6], [7, 8, 0]],
 goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
 if __name__ == '__main__':
-    # print(puzzles[0] == goal)
     depths_UC = []
     nodes_UC = []
+    time_UC = []
     depths_MisP = []
     nodes_MisP = []
+    time_MisP = []
     nodes_ManHat = []
+    time_ManHat = []
     for i in depths:
-        _, node, _, _ = a_star(puzzles[i], 1, 1000, 5, False)
+        _, node, _, t = a_star(puzzles[i], 1, 1000, 5, False, False)
         if node != -1:
             depths_UC.append(i)
             nodes_UC.append(node)
-        _, node, _, _ = a_star(puzzles[i], 2, 1000, 5, False)
+            time_UC.append(t)
+        _, node, _, t = a_star(puzzles[i], 2, 1000, 5, False, False)
         if node != -1:
             depths_MisP.append(i)
             nodes_MisP.append(node)
-        _, node, _, _ = a_star(puzzles[i], 3, 1000, 5, False)
+            time_MisP.append(t)
+        _, node, _, t = a_star(puzzles[i], 3, 1000, 5, False, False)
         if node != -1:
             nodes_ManHat.append(node)
+            time_ManHat.append(t)
 
     plt.plot(depths_UC, nodes_UC, label='UC')
     plt.plot(depths_MisP, nodes_MisP, label='MisP')
@@ -38,4 +43,12 @@ if __name__ == '__main__':
     plt.title('Nodes Expanded vs Solution Depth')
     plt.legend()
     plt.savefig('nodes_expanded_depth.jpg')
+    plt.show()
+
+    plt.plot(depths_UC, time_UC, label='UC')
+    plt.plot(depths_MisP, time_MisP, label='MisP')
+    plt.plot(depths, time_ManHat, label='ManHat')
+    plt.title('Running Time vs Solution Depth')
+    plt.legend()
+    plt.savefig('time_depth.jpg')
     plt.show()
